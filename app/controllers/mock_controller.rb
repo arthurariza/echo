13463c469590b375186ac2a59e48f568fffed8b6
arthurariza <tucaariza@gmail.com>
2022-11-16 23:59:18 -0300

    feat: add mock controller

--- app/controllers/mock_controller.rb
+++ app/controllers/mock_controller.rb
@@ -3,36 +3,39 @@
 class MockController < ApplicationController
   def mock
     endpoint = Endpoint.find_by!(verb: request.method, path: "/#{params[:mock]}")
-    render json: mock_response(request.method, params[:mock], endpoint.response)
+    render json: mock_response(endpoint.response)
   rescue ActiveRecord::RecordNotFound
     render json: error_response
   end
 
-  def mock_response(http_method, path, response)
+  def mock_response(response)
     <<~HEREDOC
-      > #{http_method} /#{path} HTTP/1.1
+      > #{request.method} /#{params[:mock]} HTTP/1.1
       > Accept: application/json
 
-      HTTP/1.1 #{response} OK
-      #{response}
+      HTTP/1.1 #{response['code']} OK
+      #{response['headers']}
 
-      { #{response} }
+      { #{response['body']} }
     HEREDOC
   end
 
   def error_response
     <<~HEREDOC
-      HTTP/1.1 404 Not found
-      Content-Type: application/vnd.api+json
+      > #{request.method} /#{params[:mock]} HTTP/1.1
+      > Accept: application/json
 
-      {
-          "errors": [
-              {
-                  "code": "not_found",
-                  "detail": "Requested Endpoint with ID `#{params[:mock]}` does not exist"
-              }
-          ]
-      }
+        HTTP/1.1 404 Not found
+        Content-Type: application/vnd.api+json
+
+        {
+            "errors": [
+                {
+                    "code": "not_found",
+                    "detail": "Requested page `#{params[:mock]}` does not exist"
+                }
+            ]
+        }
     HEREDOC
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   jsonapi_resources :endpoints
 
   get '/:mock', to: 'mock#mock'
+  post '/:mock', to: 'mock#mock'
+  patch '/:mock', to: 'mock#mock'
+  put '/:mock', to: 'mock#mock'
+  delete '/:mock', to: 'mock#mock'
 end
